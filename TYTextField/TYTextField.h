307c0e9508da2d865c1289bf45fe22a5011951ae
tongyi <ty@ty.local>
2018-05-10 14:13:40 -0700

    Initial Commit

---
+++ TYTextField/TYTextField.h
@@ -0,0 +1,19 @@
+//
+//  TYTextField.h
+//  TYTextField
+//
+//  Created by tongyi on 2018/5/10.
+//  Copyright © 2018年 tongyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TYTextField.
+FOUNDATION_EXPORT double TYTextFieldVersionNumber;
+
+//! Project version string for TYTextField.
+FOUNDATION_EXPORT const unsigned char TYTextFieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TYTextField/PublicHeader.h>
+
+
