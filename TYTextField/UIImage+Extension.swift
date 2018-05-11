bddf799956b879228a9164ea3d313d5820c90db0
tongyi <ty@ty.local>
2018-05-11 01:57:10 -0700

    version1.5

--- TYTextField/UIImage+Extension.swift
+++ TYTextField/UIImage+Extension.swift
@@ -12,9 +12,8 @@ extension UIImage {
     
     
     static var correctSymbol: UIImage? {
-        guard let bundleUrl = Bundle(for: TYTextField.self).url(forResource: "Images", withExtension: "bundle") else { return nil }
-        let bundle = Bundle(url: bundleUrl)
-        return UIImage(named: "correctSymbol.png", in: bundle, compatibleWith: nil)
+        guard let imagePath = Bundle.main.path(forResource: "correctSymbol.png", ofType: nil) else { return nil }
+        return UIImage(contentsOfFile: imagePath)
     }
     
     static var errorSymbol: UIImage? {
@@ -23,8 +22,11 @@ extension UIImage {
 //        let bundle = Bundle(url: bundleUrl)
 //        return UIImage(named: "errorSymbol.png", in: bundle, compatibleWith: nil)
         
-        guard let bundleUrl = Bundle(for: TYTextField.self).url(forResource: "Images", withExtension: "bundle") else { return nil }
-        let bundle = Bundle(url: bundleUrl)
-        return UIImage(named: "errorSymbol.png", in: bundle, compatibleWith: nil)
+//        guard let bundleUrl = Bundle(for: TYTextField.self).url(forResource: "Images", withExtension: "bundle") else { return nil }
+//        let bundle = Bundle(url: bundleUrl)
+//        return UIImage(named: "errorSymbol.png", in: bundle, compatibleWith: nil)
+        
+        guard let imagePath = Bundle.main.path(forResource: "errorSymbol.png", ofType: nil) else { return nil }
+        return UIImage(contentsOfFile: imagePath)
     }
 }
